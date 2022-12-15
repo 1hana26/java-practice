6783a187b48113e2bfba67c9d4706ec48adf8bc8
1hana26 <seolhana6206@gmail.com>
2022-12-15 09:40:19 +0900

    java 연습 2일차

---
+++ Exam09.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Exam09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("계산하고자하는 수를 차례로 입력해주세요");
+        System.out.print("첫번째 수 : ");
+        System.out.print("두번째 수 : ");
+    }
+}
